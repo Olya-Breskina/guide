c6e6f4a619ad59562ee64100c4b7b9733f9e280b
Olya-Breskina <breskina-olya@mail.ru>
2023-07-13 19:31:55 +0300

    изменила тип запросы

--- src/main/java/ru/podgoretskaya/guide/controller/InformationAboutVehicleController.java
+++ src/main/java/ru/podgoretskaya/guide/controller/InformationAboutVehicleController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.podgoretskaya.guide.dto.InformationAboutVehicleDTO;
 import ru.podgoretskaya.guide.service.InformationAboutVehicleService;
 
@@ -40,7 +37,8 @@ public class InformationAboutVehicleController {
         return new ResponseEntity<>(service.filter(model), HttpStatus.OK);
     }
 
-    @PostMapping("/guide")
+    //@PostMapping("/guide")
+    @GetMapping("/guide")
     @Operation(summary = "вся база")
     public ResponseEntity<List<InformationAboutVehicleDTO>> guide() {
         return new ResponseEntity<>(service.findAll(), HttpStatus.OK);
